2a25d6cfa20ef39676fee0eca914f2fe79818df6
tiecheng <tc@tiechengdeMacBook-Pro-2.local>
2020-12-02 19:54:07 +0800

    update

---
+++ dubbogo/client/client.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/apache/dubbo-go/common/logger"
+	"github.com/apache/dubbo-go/config"
+	gxlog "github.com/dubbogo/gost/log"
+)
+
+var (
+	survivalTimeout int = 10e9
+)
+
+func main() {
+	config.Load()
+	gxlog.CInfo("\n\n\nstart to test dubbo")
+
+	res, err := demoProvider.SayHello(context.TODO(), "tc")
+	if err != nil {
+		panic(err)
+	}
+
+	gxlog.CInfo("response result: %v\n", res)
+
+	user := User{
+		Name: "tc",
+		Age:  18,
+	}
+
+	res, err = demoProvider.SayHello2(context.TODO(), user)
+	if err != nil {
+		panic(err)
+	}
+
+	gxlog.CInfo("response result: %v\n", res)
+
+	res, err = demoProvider.SayHello3(context.TODO(), user, "tc")
+	if err != nil {
+		panic(err)
+	}
+
+	gxlog.CInfo("response result: %v\n", res)
+
+	initSignal()
+}
+
+func initSignal() {
+	signals := make(chan os.Signal, 1)
+	// It is not possible to block SIGKILL or syscall.SIGSTOP
+	signal.Notify(signals, os.Interrupt, os.Kill, syscall.SIGHUP,
+		syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	for {
+		sig := <-signals
+		logger.Infof("get signal %s", sig.String())
+		switch sig {
+		case syscall.SIGHUP:
+			// reload()
+		default:
+			time.AfterFunc(time.Duration(survivalTimeout), func() {
+				logger.Warnf("app exit now by force...")
+				os.Exit(1)
+			})
+
+			// The program exits normally or timeout forcibly exits.
+			fmt.Println("app exit now...")
+			return
+		}
+	}
+}
---
+++ dubbogo/client/demo.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	hessian "github.com/apache/dubbo-go-hessian2"
+
+	_ "github.com/apache/dubbo-go/cluster/cluster_impl"
+	_ "github.com/apache/dubbo-go/cluster/loadbalance"
+	_ "github.com/apache/dubbo-go/common/proxy/proxy_factory"
+	"github.com/apache/dubbo-go/config"
+	_ "github.com/apache/dubbo-go/filter/filter_impl"
+	_ "github.com/apache/dubbo-go/protocol/dubbo"
+	_ "github.com/apache/dubbo-go/registry/protocol"
+	_ "github.com/apache/dubbo-go/registry/zookeeper"
+)
+
+var demoProvider = new(DemoProvider)
+
+func init() {
+	config.SetConsumerService(demoProvider)
+	hessian.RegisterPOJO(&User{})
+}
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func (User) JavaClassName() string {
+	return "com.funnycode.User"
+}
+
+type DemoProvider struct {
+	SayHello  func(ctx context.Context, name string) (string, error)           `dubbo:"sayHello"`
+	SayHello2 func(ctx context.Context, user User) (string, error)             `dubbo:"sayHello"`
+	SayHello3 func(ctx context.Context, user User, name string) (string, error) `dubbo:"sayHello"`
+}
+
+func (p *DemoProvider) Reference() string {
+	return "DemoProvider"
+}
+
+func (p *DemoProvider) MethodMapper() map[string]string {
+	return map[string]string{
+		"SayHello": "SayHello",
+	}
+}
